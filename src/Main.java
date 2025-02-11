01c9e7d0f0abb4a930280f704abbad3bbbcdaa8d
CASHVP <Trimp19@gmail.com>
2025-02-11 18:36:36 +0300

    asdasdadasdsdasdasdsadas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-// dsfdsfdsfsdfs        System.out.println("Hello, World!");
+// dsfdsfdsfsdfs    sadasdasd    System.out.println("Hello, World!");
     }
 }
